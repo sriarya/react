17f3d602698c2545121e92677011fdf6805dee78
Chaithra Shree <chaithracsy@gmail.com>
2019-01-13 12:25:56 +0530

    Updates

---
+++ precache-manifest.309930e41773d7f58cceed0141cf6c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sriarya/react.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "692a06f8065e25c4efb3",
+    "url": "/sriarya/react.git/static/js/runtime~main.692a06f8.js"
+  },
+  {
+    "revision": "dfa0b33762c11839e1e5",
+    "url": "/sriarya/react.git/static/js/main.dfa0b337.chunk.js"
+  },
+  {
+    "revision": "247819375e38dac50150",
+    "url": "/sriarya/react.git/static/js/1.24781937.chunk.js"
+  },
+  {
+    "revision": "dfa0b33762c11839e1e5",
+    "url": "/sriarya/react.git/static/css/main.aa9d2d82.chunk.css"
+  },
+  {
+    "revision": "247819375e38dac50150",
+    "url": "/sriarya/react.git/static/css/1.fc0133ba.chunk.css"
+  },
+  {
+    "revision": "00b02dfd369f4ca5c24ea112c92f8c42",
+    "url": "/sriarya/react.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sriarya/react.git/precache-manifest.309930e41773d7f58cceed0141cf6c9d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sriarya/react.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
